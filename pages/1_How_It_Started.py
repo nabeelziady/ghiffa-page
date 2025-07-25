f5b996cddade650ddb75d2993a4bcb56b49c8258
nabeelziady <nabeelziady31@gmail.com>
2025-07-25 14:49:06 +0700

    Update 1_How_It_Started.py

--- pages/1_How_It_Started.py
+++ pages/1_How_It_Started.py
@@ -13,4 +13,4 @@ st.subheader("DMs All The Way")
 st.write("It was the 12th of March 2025, when Nabeel started his shot with 'lucu kak gambarnya' at 10 pm." \
 " It is the only DM that nabeel will never regret because it led him to such a crazy bag. He was really nervous at first, but ghiffa's replies was so adorable it made nabeel sooooo happy." \
 " Nabeel's first impression was he didn't expect that she was this funny and her jokes matches with him." \
-" Until today, Nabeel still remembers the day that he will never ever regret in his life, the day that turned everything becomes beautiful.")
+" Until today, Nabeel still remembers the day that he will never ever regret in his life, the day that turned everything differently.")
